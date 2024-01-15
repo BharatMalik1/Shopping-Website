f67c9ea547ca5a96ec4ce1feeec2adffc8025b1c
BharatMalik1 <105875249+BharatMalik1@users.noreply.github.com>
2024-01-16 03:18:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,71 +1,19 @@
-// importing the module http
-const http = require('http');
-const queryString = require('querystring')
-const fs = require('fs');
-// const port = process.env.PORT || 1337;
-const server = http.createServer(function(req,res){
-    console.log("request listener function");
-    console.log(req.url);
-    // res.setHeader("Content-Type","text/plain");
-    // res.write("Hi from the server");
-    // res.setHeader("Content-Type","application/json")
-    // res.write(JSON.stringify({name:"Bill",age:23}))
-    // res.end("Hi from the server");
-    if(req.url === "/"){
-        respondText(req,res);
-    } else if(req.url === "/person"){
-        respondJson(req,res);
-    } else if(req.url.match(/^\/converttouppercase/)){
-        respondConvert(req,res);
-    } else if(req.url.match(/^\/files/)){
-        respondFile(req,res)
-    }else{
-        respondNothing(req,res);
-    } 
-    // res.end();
-})
-server.listen(3000,()=>{
-    console.log("starting the server");
-});
-// console.log(`server running on port ${port}`);
+const express = require('express');
 
+const port = 3000;
+const app = express();
+app.get("/",respondText);
+app.get("/json",respondJson)
+app.get("*",respondNotFound)
 function respondText(req,res){
-    res.setHeader("Content-Type","text/plain");
-    res.end("Hi from the server");
+  res.send("HI from express server");  
 }
-
 function respondJson(req,res){
-    res.setHeader("Content-Type","application/json")
-    res.end(JSON.stringify({name:"Bill",age:23}))
+    res.json({name:"Ben thomas",age:23})
 }
-
-function respondNothing(req,res){
-    res.writeHead(404,"Nothing found");
-    res.end("Nothing found");
+function respondNotFound(req,res){
+    res.send("Page you are trying to access is not available")
 }
-
-function respondConvert(req,res){
-    // console.log(req.url);// /converttouppercase?input=fsdfs&query=sdff
-    // console.log(req.url.split("?"));
-    // [ '/converttouppercase', 'input=fsdfs&query=sdff' ]
-    // console.log(req.url.split("?").slice(1)); 
-    // [ 'input=fsdfs&query=sdff' ]
-    // console.log(req.url.split("?").slice(1).join(""));
-    // input=fsdfs&query=sdff
-    // console.log(queryString.parse(req.url.split("?").slice(1).join("")));
-    const {input} = queryString.parse(req.url.split("?").slice(1).join(""));
-    // { input: 'fsdfs', query: 'sdff' }
-    res.end(input.toUpperCase());
-}
-
-function respondFile(req,res){
-    // console.log(req.url.split("/files"));
-    // const filename = req.url.split("/files")[1];
-    // console.log(filename);
-
-    const filename = `${__dirname}/public${req.url.split("/files")[1]}`
-    console.log(filename);
-    fs.createReadStream(filename)
-        .on('error',()=>{respondNothing(req,res)})
-        .pipe(res)
-}
+app.listen(port,()=>{
+    console.log("Listening on port 3000");
+});
