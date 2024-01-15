e0dce6d72fe8f310e81bd86a5ef572909ea02a8c
BharatMalik1 <105875249+BharatMalik1@users.noreply.github.com>
2024-01-16 03:18:01 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,10 +1,50 @@
-// inbuilt modules
-const os = require('os');
-//Global variables
-console.log(__dirname);
-console.log(__filename);
-//Global objects
-console.log(process.platform);
-// imported module 'os'
-console.log(os.platform());
-console.log(os.release());
+console.log("First message");
+setTimeout(()=>{
+    console.log("Second message");
+},1000)
+console.log("Third message");
+
+function a(){
+    console.log("a called");
+    // throw new Error("an error");
+}
+function b(){
+    a();
+}
+function c(){
+    b();
+}
+c();
+
+// function d(){
+//     d();
+// }
+
+// d();
+
+
+function aFunc(message){
+    console.log("Do something");
+    message();
+}
+
+function callback(){
+    console.log("callback func");
+}
+aFunc(callback);
+
+// callback hell
+function callbackHell(){
+    setTimeout(()=>{
+        const data = {user:"admin"}
+        console.log(data);
+        setTimeout(()=>{
+            data.role = "administrator";
+            console.log(data);
+            setTimeout(()=>{
+                console.log("do something");
+            },500)
+        },1000)   
+    },500)
+}
+callbackHell();
